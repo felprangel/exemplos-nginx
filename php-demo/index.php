52f1723511213291ecbae727dff90bf7215b899f
Felipe Rangel <113402519+felprangel@users.noreply.github.com>
2025-03-30 11:03:18 -0300

    remove acentos

--- php-demo/index.php
+++ php-demo/index.php
@@ -5,5 +5,5 @@ header('Content-type: application/json');
 http_response_code(200);
 echo json_encode([
     'status' => 'success',
-    'message' => 'Você está aprendendo NGINX!',
+    'message' => 'Voce esta aprendendo NGINX!',
 ]);
